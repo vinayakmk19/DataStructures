0ab2bc26dc8aea970f01e719467d07e3c49200bd
VINAYAK KAMBLE <vinayakkamble963@gmail.com>
2022-07-27 17:40:25 +0530

    changes are done

--- LinkedList/src/com/abc/package1/RemoveConsicutive.java
+++ LinkedList/src/com/abc/package1/RemoveConsicutive.java
@@ -1,9 +1,14 @@
 package com.abc.package1;
 
+import java.util.LinkedList;
+
 public class RemoveConsicutive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList<Integer> ll = new LinkedList<Integer>();
+		ll.add(10);
+		ll.add(20);
+		System.out.println(ll);
 
 	}
 
