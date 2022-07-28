ea657749b38c2b2990cd4c89965301e084c9cf67
vinayakmk19 <110101863+vinayakmk19@users.noreply.github.com>
2022-07-28 22:27:38 +0530

    Add files via upload

---
+++ LinkedList/RemoveConsicutiveDuplicate.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class RemoveConsicutiveDuplicate {
+
+	public static void main(String[] args) {
+		
+LinkedList<Integer> ll = new LinkedList<Integer>();
+
+	}
+
+}
