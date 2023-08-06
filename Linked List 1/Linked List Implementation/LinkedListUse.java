87da3d3fa0a5598171a2f6f14173896c01671f95
vinayakmk19 <110101863+vinayakmk19@users.noreply.github.com>
2023-08-06 07:43:35 +0530

    Update LinkedListUse.java

--- Linked List 1/Linked List Implementation/LinkedListUse.java
+++ Linked List 1/Linked List Implementation/LinkedListUse.java
@@ -147,24 +147,22 @@ public class LinkedListUse {
 	}
 	
 	public static Node<Integer> deleteNode(Node<Integer> head, int i) {
-		 if(head==null )
-           return head;
-       if(i==0)
-           return head.next;
-       int count=0;
-     Node<Integer> temp=head;
-       while(temp!=null && count<i-1)
-       {
-           temp=temp.next;
-           count++;
-       }
-       if(temp==null)
-           return head;
-       if(temp.next!=null)
-           temp.next=temp.next.next;
-        
-       
-       return head;
+		if(head==null )
+           		return head;
+       		if(i==0)
+           		return head.next;
+       		int count=0;
+    		Node<Integer> temp=head;
+       		while(temp!=null && count<i-1)
+       		{
+	           temp=temp.next;
+	           count++;
+       		}
+       		if(temp==null)
+           		return head;
+       		if(temp.next!=null)
+           		temp.next=temp.next.next;
+        		return head;
 	}
 
 	public static void main(String[] args) {
